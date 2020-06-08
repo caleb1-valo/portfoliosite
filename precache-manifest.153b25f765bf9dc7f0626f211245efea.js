256e4e95259fe3d30c938217fab45f79c6d7ea86
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2020-06-08 22:44:22 +0100

    Updates

---
+++ precache-manifest.153b25f765bf9dc7f0626f211245efea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320a46082163727f4b321101551aa68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311b0f799283b3395042",
+    "url": "/static/css/main.782423f7.chunk.css"
+  },
+  {
+    "revision": "f74679e95ad3d4b93fea",
+    "url": "/static/js/2.bb477439.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb477439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311b0f799283b3395042",
+    "url": "/static/js/main.29f570ad.chunk.js"
+  },
+  {
+    "revision": "ddcb41f20439bb13e4d5",
+    "url": "/static/js/runtime-main.07387012.js"
+  },
+  {
+    "revision": "95ae34052e9f08767319c3f448d14e8d",
+    "url": "/static/media/front end.95ae3405.jpg"
+  },
+  {
+    "revision": "50e97f47972ef3bc792015763042c3a8",
+    "url": "/static/media/plain-white-background.50e97f47.jpg"
+  },
+  {
+    "revision": "3e2772f1d67647741499c715edbcd4a0",
+    "url": "/static/media/valosky.3e2772f1.jpg"
+  }
+]);
